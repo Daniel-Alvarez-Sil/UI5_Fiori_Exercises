670c3fa47e36952ca2714120b6cfbe6160ca102b
Daniel-Alvarez-Sil <mision.d.a.s.04@gmail.com>
2025-04-14 04:22:11 -0600

    Addition of Exercises 1-4

---
+++ 1.Desarrollo_de_Aplicaciones/webapp/index.js
@@ -0,0 +1,27 @@
+// // Segunda y Tercera Iteración 
+sap.ui.define([
+	"sap/ui/core/mvc/XMLView"
+], (XMLView) => {
+	"use strict";
+
+	XMLView.create({
+		viewName: "ui5.quickstart.App"
+	}).then((oView) => oView.placeAt("content"));
+});
+
+
+// // Primera Iteración 
+// sap.ui.define([
+// 	"sap/m/Button",
+// 	"sap/m/MessageToast"
+// ], (Button, MessageToast) => {
+// 	"use strict";
+
+// 	new Button({
+// 		text: "Ready...",
+// 		press() {
+// 			MessageToast.show("Hello World!");
+// 		}
+// 	}).placeAt("content");
+
+// });
