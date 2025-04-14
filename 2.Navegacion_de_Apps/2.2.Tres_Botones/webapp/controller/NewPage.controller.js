b8e92af5bd443994e74d79dd97ec954ffcba2c80
Daniel-Alvarez-Sil <mision.d.a.s.04@gmail.com>
2025-04-14 14:26:42 -0600

    Modification of Exercise 2 into two subdivisions

---
+++ 2.Navegacion_de_Apps/2.2.Tres_Botones/webapp/controller/NewPage.controller.js
@@ -0,0 +1,13 @@
+// Controller for New Page, extends BaseController
+sap.ui.define([
+	"sap/ui/demo/nav/controller/BaseController"
+], function (BaseController) {
+	"use strict";
+
+	return BaseController.extend("sap.ui.demo.nav.controller.NotFound", {
+
+		
+
+	});
+
+});
